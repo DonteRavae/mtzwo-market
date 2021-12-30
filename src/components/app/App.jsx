3e011bfa2c696f07c361016f5de59c2fe2d00a69
Donte Ravae <DonteRavae@gmail.com>
2021-12-30 01:47:33 -0500

    extracted app to components folder

---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AppHeader from "../app-header/AppHeader";
+
+const App = () => {
+    return (
+        <main className="App">
+            {/* Header */}
+            <AppHeader />
+            {/* Routes between pages */}
+            
+        </main>
+    );
+}
+
+export default App;
