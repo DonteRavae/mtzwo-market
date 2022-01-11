a340e1f86055a862538f14c74c5a3c78243aff47
Donte Ravae <DonteRavae@gmail.com>
2022-01-11 13:32:34 -0500

    added routing to logo to navigate to root path

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import logoImg from "../../assets/mtzwo-logo.png";
 
 const Logo = ({ className }) => {
+  const navigate = useNavigate();
+  const handleClick = () => navigate("/");
   return (
-    <div className={className}>
+    <div className={className} onClick={handleClick}>
       <img src={logoImg} alt="Mt. Zion Shield Logo" />
     </div>
   );
