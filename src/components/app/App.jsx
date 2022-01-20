db01812277f00ee62cbfec44788a18396f276f9b
Donte Ravae <DonteRavae@gmail.com>
2022-01-19 20:45:25 -0500

    added MobileMenu component w/ MobileMenuItems components to App component below AppHeader component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import AppHeader from "../AppHeader/AppHeader";
+import StyledMobileMenu, {
+  StyledMobileMenuItem,
+} from "../MobileMenu/MobileMenu.styled";
 
 const App = () => {
+  const [isMobileMenuActive, setIsMobileMenuActive] = useState(false);
+  const toggleMobileMenu = () => {
+    setIsMobileMenuActive(!isMobileMenuActive);
+  };
   return (
     <main className="App">
       {/* Header */}
-      <AppHeader />
+      <AppHeader toggleMobileMenu={toggleMobileMenu} isMobileMenuActive={isMobileMenuActive} />
+      <StyledMobileMenu isActive={isMobileMenuActive}>
+        <StyledMobileMenuItem title="New Releases" />
+        <StyledMobileMenuItem title="Audio" />
+        <StyledMobileMenuItem title="Video" />
+        <StyledMobileMenuItem title="Books" />
+        <StyledMobileMenuItem title="Clothing" />
+        <StyledMobileMenuItem title="Deals" />
+        <StyledMobileMenuItem title="Account" accountItem toggleMenu={toggleMobileMenu}/>
+      </StyledMobileMenu>
       {/* Routes between pages */}
     </main>
   );
