301644bc9dabc0bcefec95d86beb61f745d61995
Donte Ravae <DonteRavae@gmail.com>
2022-01-20 21:02:11 -0500

    changed Sign In/Sign Up button background colors to secondary constant

--- src/components/MobileMenu/MobileMenuItem/MobileMenuItem.styled.jsx
+++ src/components/MobileMenu/MobileMenuItem/MobileMenuItem.styled.jsx
@@ -10,26 +10,20 @@ const StyledMobileMenuItem = styled(MobileMenuItem)`
     padding: 0 1.5rem;
     cursor: pointer;
     border: 1px solid transparent;
-    border-bottom: 1px solid;
-
+    border-bottom: 1px solid white;
     h3 {
       margin-right: auto;
     }
-
     &.account-item {
       flex-direction: column;
       border: 1px solid transparent;
       cursor: default;
-      // background-color: #444;
-
       hr {
         width: 95%;
       }
-
       .account-menu-items {
         width: 100%;
         margin: 1rem 0;
-
         .account-menu-item {
           width: 90%;
           display: flex;
@@ -37,44 +31,36 @@ const StyledMobileMenuItem = styled(MobileMenuItem)`
           justify-content: space-around;
           margin: 0 auto;
           border: 1px solid transparent;
-
           .account-menu-item-icon {
             margin-left: auto;
           }
-
           h3 {
             margin-left: 0.75rem;
             margin-right: auto;
           }
-
           &:hover {
             background-color: ${THEME.PRIMARY};
             border: 1px solid white;
           }
         }
-
         &.guest {
           display: flex;
           padding: 0;
           justify-content: space-evenly;
-
           button {
             height: 40px;
             width: 30%;
             border: none;
             border-radius: 2px;
             outline: none;
-            background-color: gold;
+            background-color: ${THEME.SECONDARY};
             color: ${THEME.PRIMARY};
-
             span {
               margin-right: 0.5rem;
-
               svg {
                 filter: drop-shadow(1px 1px #333);
               }
             }
-
             &:hover {
               background-color: ${THEME.PRIMARY};
               color: white;
@@ -84,12 +70,9 @@ const StyledMobileMenuItem = styled(MobileMenuItem)`
         }
       }
     }
-
     &:hover {
-      border: 1px solid white;
       background-color: ${THEME.PRIMARY};
       color: white;
-
       &.account-item {
         background-color: #333;
         border: 1px solid transparent;
