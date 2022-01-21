6ee0823b659326c16e7af2dd9a04ba5defe1e484
Donte Ravae <DonteRavae@gmail.com>
2022-01-20 19:32:46 -0500

    added more breakpoints for smaller devices

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,17 @@
 export const SIZES = {
+  XXSMALL: "390px",
+  XSMALL: "500px",
   SMALL: "576px",
   SMEDIUM: "577px",
   MEDIUM: "768px",
-  LARGE: "992px",
+  LARGE: "993px",
   XLARGE: "1200px",
   XXLARGE: "1400px",
 };
 
 export const DEVICES = {
+  XXSMALL: `(max-width: ${SIZES.XXSMALL})`,
+  XSMALL: `(max-width: ${SIZES.XSMALL})`,
   SMALL: `(max-width: ${SIZES.SMALL})`,
   SMEDIUM: `(max-width: ${SIZES.SMEDIUM})`,
   MEDIUM: `(max-width: ${SIZES.MEDIUM})`,
