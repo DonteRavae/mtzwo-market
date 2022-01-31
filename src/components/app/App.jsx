76450eede4185c4fa7ae42de1a2cf6a71a6a5492
Donte Littlejohn <donteravae@gmail.com>
2022-01-31 05:19:30 -0500

    added submenu items and routing destinations for header

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -11,15 +11,145 @@ const App = () => {
   return (
     <main className="App">
       {/* Header */}
-      <AppHeader toggleMobileMenu={toggleMobileMenu} isMobileMenuActive={isMobileMenuActive} />
-      <StyledMobileMenu isActive={isMobileMenuActive} toggleMobileMenu={toggleMobileMenu}>
-        <StyledMobileMenuItem title="New Releases" />
-        <StyledMobileMenuItem title="Audio" sub/>
-        <StyledMobileMenuItem title="Video" sub/>
-        <StyledMobileMenuItem title="Books" />
-        <StyledMobileMenuItem title="Clothing" sub/>
-        <StyledMobileMenuItem title="Deals" sub/>
-        <StyledMobileMenuItem title="Account" accountItem toggleMenu={toggleMobileMenu}/>
+      <AppHeader
+        toggleMobileMenu={toggleMobileMenu}
+        isMobileMenuActive={isMobileMenuActive}
+      />
+      <StyledMobileMenu
+        isActive={isMobileMenuActive}
+        toggleMobileMenu={toggleMobileMenu}
+      >
+        <StyledMobileMenuItem title="New Releases" submenu>
+          <StyledMobileMenuItem
+            title="Audio"
+            toggleMenu={toggleMobileMenu}
+            destination="newreleases/audio"
+          />
+          <StyledMobileMenuItem
+            title="Video"
+            toggleMenu={toggleMobileMenu}
+            destination="newreleases/video"
+          />
+          <StyledMobileMenuItem
+            title="Podcasts"
+            toggleMenu={toggleMobileMenu}
+            destination="newreleases/podcasts"
+          />
+        </StyledMobileMenuItem>
+        <StyledMobileMenuItem title="Audio" submenu>
+          <StyledMobileMenuItem
+            title="Sunday Service"
+            toggleMenu={toggleMobileMenu}
+            destination="audio/sundayservice"
+          />
+          <StyledMobileMenuItem
+            title="Wednesday Bible Study"
+            toggleMenu={toggleMobileMenu}
+            destination="audio/biblestudy"
+          />
+          <StyledMobileMenuItem
+            title="Special Guests"
+            toggleMenu={toggleMobileMenu}
+            destination="audio/specialguests"
+          />
+          <StyledMobileMenuItem
+            title="Podcasts"
+            toggleMenu={toggleMobileMenu}
+            destination="audio/podcasts"
+          />
+          <StyledMobileMenuItem
+            title="All Audio"
+            toggleMenu={toggleMobileMenu}
+            destination="audio/"
+          />
+        </StyledMobileMenuItem>
+        <StyledMobileMenuItem title="Video" submenu>
+          <StyledMobileMenuItem
+            title="Sunday Service"
+            toggleMenu={toggleMobileMenu}
+            destination="video/sundayservice"
+          />
+          <StyledMobileMenuItem
+            title="Wednesday Bible Study"
+            toggleMenu={toggleMobileMenu}
+            destination="video/biblestudy"
+          />
+          <StyledMobileMenuItem
+            title="Special Guests"
+            toggleMenu={toggleMobileMenu}
+            destination="video/specialguests"
+          />
+          <StyledMobileMenuItem
+            title="Podcasts"
+            toggleMenu={toggleMobileMenu}
+            destination="video/podcasts"
+          />
+          <StyledMobileMenuItem
+            title="Performances"
+            toggleMenu={toggleMobileMenu}
+            destination="video/performances"
+          />
+          <StyledMobileMenuItem
+            title="All Video"
+            toggleMenu={toggleMobileMenu}
+            destination="video"
+          />
+        </StyledMobileMenuItem>
+        <StyledMobileMenuItem title="Books" toggleMenu={toggleMobileMenu} />
+        <StyledMobileMenuItem title="Clothing" submenu>
+          <StyledMobileMenuItem
+            title="Men"
+            toggleMenu={toggleMobileMenu}
+            destination="clothing/men"
+          />
+          <StyledMobileMenuItem
+            title="Women"
+            toggleMenu={toggleMobileMenu}
+            destination="clothing/women"
+          />
+          <StyledMobileMenuItem
+            title="Kids"
+            toggleMenu={toggleMobileMenu}
+            destination="clothing/kids"
+          />
+          <StyledMobileMenuItem
+            title="Hats"
+            toggleMenu={toggleMobileMenu}
+            destination="clothing/hats"
+          />
+          <StyledMobileMenuItem
+            title="All Clothes"
+            toggleMenu={toggleMobileMenu}
+            destination="clothing"
+          />
+        </StyledMobileMenuItem>
+        <StyledMobileMenuItem title="Deals" submenu>
+          <StyledMobileMenuItem
+            title="Deals of the Week"
+            toggleMenu={toggleMobileMenu}
+            destination="deals/weeklydeals"
+          />
+          <StyledMobileMenuItem
+            title="Member Deals"
+            toggleMenu={toggleMobileMenu}
+            destination="deals/memberdeals"
+          />
+          <StyledMobileMenuItem
+            title="Top Deals"
+            toggleMenu={toggleMobileMenu}
+            destination="deals/topdeals"
+          />
+          <StyledMobileMenuItem
+            title="Gift Ideas"
+            toggleMenu={toggleMobileMenu}
+            destination="deals/gifts"
+          />
+        </StyledMobileMenuItem>
+        <StyledMobileMenuItem
+          title="Account"
+          accountItem
+          toggleMenu={toggleMobileMenu}
+        />
       </StyledMobileMenu>
       {/* Routes between pages */}
     </main>
