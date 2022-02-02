1e8ff06a466d67f017ab002fafdeb76f392c7d9c
Donte Ravae <DonteRavae@gmail.com>
2022-02-01 21:40:00 -0500

    added navigation routing to deals page links in the AppHeader component

--- src/components/DealsPageLink/DealsPageLink.jsx
+++ src/components/DealsPageLink/DealsPageLink.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const DealsPageLink = ({ className, title }) => {
+
+const DealsPageLink = ({ className, title, destination }) => {
+  const navigate = useNavigate();
+  const navigateToPage = () => {
+    navigate(`/${destination}`)
+  }
   return (
-    <div className={className}>
+    <div className={className} onClick={navigateToPage}>
       <span>{title}</span>
     </div>
   );
